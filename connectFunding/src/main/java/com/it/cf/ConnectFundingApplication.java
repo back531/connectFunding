8b4091fac12c769e1460f27613d049748a055026
back531 <back531@naver.com>
2021-07-12 15:46:11 +0900

    commit

---
+++ connectFunding/src/main/java/com/it/cf/ConnectFundingApplication.java
@@ -0,0 +1,13 @@
+package com.it.cf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectFundingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectFundingApplication.class, args);
+	}
+
+}
---
+++ connectFunding/src/test/java/com/it/cf/ConnectFundingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.cf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectFundingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
