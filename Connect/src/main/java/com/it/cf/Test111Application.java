3abb6a612e636ae710aacf25d3259d8ab6d081e8
back531 <back531@naver.com>
2021-07-12 16:22:01 +0900

    commit

---
+++ Connect/src/main/java/com/it/cf/Test111Application.java
@@ -0,0 +1,13 @@
+package com.it.cf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test111Application.class, args);
+	}
+
+}
